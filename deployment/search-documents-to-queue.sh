18b40f8a70928bda625d2fcfe303226e770a46ab
Matt Debont <matt.debont@jncc.gov.uk>
2019-05-21 09:24:57 +0100

    Update search-documents-to-queue.sh

--- deployment/search-documents-to-queue.sh
+++ deployment/search-documents-to-queue.sh
@@ -4,7 +4,7 @@ shopt -s globstar
 for i in $1; do
         echo "Processing $i"
         BODY=`cat $i`        
-        retVal=$(aws sqs send-message --queue-url $2 --message-body "$BODY")
+        retVal=`aws sqs send-message --queue-url $2 --message-body "$BODY" | echo $?`
         if [ $retVal -ne 0 ]; then
             echo "An error occured"
             exit $retVal
