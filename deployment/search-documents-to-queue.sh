3453b8977b0e66aac66d7abaf8f312fcc4d3df01
Matt Debont <matt.debont@jncc.gov.uk>
2019-05-21 09:11:46 +0100

    Update search queue pusher script to check for failure of run commands

--- deployment/search-documents-to-queue.sh
+++ deployment/search-documents-to-queue.sh
@@ -3,6 +3,10 @@
 shopt -s globstar
 for i in $1; do
         echo "Processing $i"
-        BODY=`cat $i`
-        aws sqs send-message --queue-url $2 --message-body "$BODY"
+        BODY=`cat $i`        
+        retVal=aws sqs send-message --queue-url $2 --message-body "$BODY"
+        if [ $retVal -ne 0 ]; then
+            echo "An error occured"
+            exit $retVal
+        fi
 done
