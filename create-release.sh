12fa1c910533402c534c884e556f139b187a836a
felix mason <felix.mason@jncc.gov.uk>
2019-05-31 15:19:45 +0100

    make releses not draft

--- create-release.sh
+++ create-release.sh
@@ -75,7 +75,7 @@ payload=$(
      --arg tag "$TAG" \
      --arg name "$NAME" \
      --arg body "$BODY" \
-     '{ tag_name: $tag, name: $name, body: $body, draft: true }'
+     '{ tag_name: $tag, name: $name, body: $body, draft: false }'
 )
 
 response=$(
