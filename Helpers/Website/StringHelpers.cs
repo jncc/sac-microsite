aca29d83756b0a34aa239eda098a945492f0c27f
Matt Debont <matt.debont@jncc.gov.uk>
2019-06-19 09:57:56 +0100

    Added anchor tag fixing function

--- Helpers/Website/StringHelpers.cs
+++ Helpers/Website/StringHelpers.cs
@@ -8,6 +8,18 @@ namespace JNCC.Microsite.SAC.Helpers.Website
         public static string RemoveHTMLTags(string input, string replaceWith = "")
         {
             return Regex.Replace(input, "<[^>]*>", replaceWith);
-        }     
+        }
+
+        public static string FixHTMLString(string input) {
+            return StringHelpers.FixAnchorTags(input);
+        }
+
+        public static string FixAnchorTags(string input)
+        {
+            input = Regex.Replace(input, "<[Aa] href=&quot;", "<a targe=\"_blank\" href=\"");
+            input = Regex.Replace(input, "&quot;>", "\">");
+            input = Regex.Replace(input, "</A>", "</a>");
+            return input;
+        }
     }
 }
